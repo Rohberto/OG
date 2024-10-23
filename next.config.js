98fae1f8bf3a3907b2b06437494b99cf09c65a97
Robert <oluwaseunrobert44@gmail.com>
2024-10-23 22:41:50 +0100

    Zoho mail

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+      source: '/zohoverify/verifyforzoho',
+      destination: '/zohoverify/verifyforzoho.html',
+      permanent: true
+      }
+    ]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
